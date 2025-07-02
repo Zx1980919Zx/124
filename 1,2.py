c6d19e60c69ec4c819c2b3551579b0d4aac75694
Sergei <140450497+Zx1980919Zx@users.noreply.github.com>
2025-07-02 22:01:30 +0300

    Add files via upload

---
+++ 1,2.py
@@ -0,0 +1,16 @@
+def calculate_number(input_number):
+    num_str = str(46275)
+    
+    
+    tt = int(num_str[4])
+    th = int(num_str[6])
+    h = int(num_str[2])
+    d = int(num_str[7])
+    u = int(num_str[5])
+    
+    
+    numerator = 7**5*2
+    denominator = 4-6
+    
+    result = numerator / denominator
+    return result
