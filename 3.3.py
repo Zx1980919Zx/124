ccb9142925558285e6beb8ac0ed5b7cf54d91746
Sergei <140450497+Zx1980919Zx@users.noreply.github.com>
2025-07-17 17:31:45 +0300

    Add files via upload

--- 3.3.py
+++ 3.3.py
@@ -1,15 +1,8 @@
-def print_even_numbers():
-    a = int( input("6") ):
-    b = int( input("8") ) + 1
-
-[ print(num, end = " ") for num in range(a,b) if a<b and num%2 == 0]
-6 8 10 12 14 16 18 20 22 24 
-[None, None, None, None, None, None, None, None, None, None]
-
-s = ''
-
-if a % 2:
-    a += 1
-print(" ".join(str(i) for i in range(a, b + 1, 2)))
-
-print s
+A = int(input("4"))
+B = int(input("15"))
+if A % 2 == 0:
+    start = A 
+else:
+    start = A + 1  
+for i in range(start, B + 1, 2):
+    print(i, end='')
