e339327ab944a24269ac3f82c20b33575595b2c7
Sergei <140450497+Zx1980919Zx@users.noreply.github.com>
2025-07-17 13:47:10 +0300

    Add files via upload

--- 1.py
+++ 1.py
@@ -1,17 +1,6 @@
-def perimeter(a,b):
-    res=(a+b)*2
-    return res
-    p=perimeter(a,b)
-  
- 
-def area(a, b):
-    res = a*b
-    return res
-    s=area(a,b)
- 
- 
- 
-a = int(input("4"))
-b = int(input("9"))
-print("Периметр =",perimeter(4,9))
-print("Площадь =",area(4,9))
+a=int(input(2))
+b=int(input(7))
+P=2*(2+7)    
+S=2*7
+print(P) 
+print(S)
