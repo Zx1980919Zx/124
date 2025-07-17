f7d89825be443938c71a12719a01e00fd0981c15
Sergei <140450497+Zx1980919Zx@users.noreply.github.com>
2025-07-17 17:31:05 +0300

    Add files via upload

--- 3.1.py
+++ 3.1.py
@@ -1,15 +1,7 @@
-def count_zeros():
-
-    N = int(input("2:"))
-
-    zero_count = 0
-
-    print("4:")
-    for _ in range(N):
-        number = int(input(2))
-        if number == 0:
-            zero_count += 1  
-
-    print("0")
-
-count_zeros()
+N = int(input("10,0,-5,0,12"))
+zero_count = 0
+for i in range(N)
+num = int(input("10,0,-5,0,12 {i+1}"))
+if num == 0:
+zero_count += 1
+print(zero_count)
